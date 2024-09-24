b4db673d4a14d3f926f6dfe8285ffe466c1339e9
ImtiazAhmed01 <imtiazahm21@gmail.com>
2024-09-24 12:55:56 -0500

    done?

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 document.getElementById('historybtn').addEventListener('click', function () {
-    this.style.backgroundColor = '#B4F461';
-    document.getElementById('cbtn').style.backgroundColor = '#e2e8f0';
-    showHistory('historyWillBeWrittenHere')
+
+    document.getElementById('cbtn').classList.remove('bg-[#B4F461]', 'text-black');
+    document.getElementById('cbtn').classList.add('bg-white', 'hover:bg-gray-300');
+    this.classList.remove('bg-white', 'hover:bg-gray-300');
+    this.classList.add('bg-[#B4F461]', 'text-black');
+    showHistory('historyWillBeWrittenHere');
 });
+
 document.getElementById('cbtn').addEventListener('click', function () {
-    this.style.backgroundColor = '#B4F461';
-    document.getElementById('historybtn').style.backgroundColor = '';
-    showHistory('dcard')
+    document.getElementById('historybtn').classList.remove('bg-[#B4F461]', 'text-black');
+    document.getElementById('historybtn').classList.add('bg-white', 'hover:bg-gray-300');
+    this.classList.remove('bg-white', 'hover:bg-gray-300');
+    this.classList.add('bg-[#B4F461]', 'text-black');
+    showHistory('dcard');
 });
 
 
@@ -14,43 +20,18 @@ let amountLeft = parseFloat(document.getElementById('mainAmount').innerText);
 
 
 document.getElementById('dbtn1').addEventListener('click', function () {
-    // loca, why
-    cal('dinput', 'noaDonate')
-    mainCal('dinput', 'noaDonate', 'mainAmount', 'Flood', 'Noakhali')
-    // input.forEach(input => input.value = '');
+    cal('dinput', 'noaDonate');
+    mainCal('dinput', 'noaDonate', 'mainAmount', 'Flood', 'Noakhali');
 });
 document.getElementById('dbtn2').addEventListener('click', function () {
-
-    cal('dinput2', 'feniAmount')
-    mainCal('dinput2', 'feniAmount', 'mainAmount', 'Flood Relif', 'Feni')
+    cal('dinput2', 'feniAmount');
+    mainCal('dinput2', 'feniAmount', 'mainAmount', 'Flood Relif', 'Feni');
 });
 document.getElementById('dbtn3').addEventListener('click', function () {
-
-    cal('dinput3', 'quotaAmout')
-    mainCal('dinput3', 'quotaAmout', 'mainAmount', 'Aid for Injured', ' Quota Movement')
+    cal('dinput3', 'quotaAmout');
+    mainCal('dinput3', 'quotaAmout', 'mainAmount', 'Aid for Injured', ' Quota Movement');
 });
 
 
 
 
-// document.getElementById('historybtn').addEventListener('click', function () {
-
-//     this.classList.add('bg-[#B4F461]');
-//     document.getElementById('cbtn').classList.remove('bg-[#B4F461]');
-//     document.getElementById('cbtn').classList.add('bg-[#e2e8f0]');
-//     showHistory('historyWillBeWrittenHere');
-// });
-
-// document.getElementById('cbtn').addEventListener('click', function () {
-
-//     this.classList.add('bg-[#B4F461]');
-//     document.getElementById('historybtn').classList.remove('bg-[#B4F461]');
-//     document.getElementById('historybtn').classList.add('bg-[#e2e8f0]');
-
-//     showHistory('dcard')
-// });
-
-// timezone need to recap
-// const currentDate = new Date();
-// const forDate = currentDate.toString();
-// const time=("Date:", forDate);
